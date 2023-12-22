c359448889b2d0a6bf5ff0fa3cc21638006463c2
Muhammad Hammad <hammadarshad834@gmail.com>
2023-12-22 08:53:12 -0500

    Initial Commit

---
+++ IndeedClone/IndeedCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndeedCloneApp.swift
+//  IndeedClone
+//
+//  Created by Muhammad Hammad on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct IndeedCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
